4dd588ea902790fb6da1173e6e76fb0d57b47d21
Joychema <joymelanin19@gmail.com>
2025-08-19 08:42:14 +0300

    functions assignment submission

--- functions.py
+++ functions.py
@@ -1,6 +1,12 @@
 # functions.py
 
-
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        discount_amount = price * (discount_percent / 100)
+        final_price = price - discount_amount
+        return final_price
+    else:
+        return price
     
 # Prompt the user for input
 original_price = float(input("Enter the original price of the item: "))
